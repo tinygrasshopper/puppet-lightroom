8421d52edda6ee2b711d67e0cc0f41de5f45a8e8
Jatin Naik <jsnaik@gmail.com>
2014-06-24 00:53:48 +0200

    Added the lightroom class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,11 @@
-# This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+# Public: Install Lightroom to /Applications.
+#
+# Examples
+#
+#   include lightroom
+class lightroom {
+  package { 'Lightroom':
+    provider => 'appdmg',
+    source   => 'http://download.adobe.com/pub/adobe/lightroom/mac/5.x/Lightroom_5_LS11_mac_5_4.dmg',
+  }
 }
---
+++ spec/classes/lightroom_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe 'lightroom' do
+  it do
+    should contain_package('Lightroom').with(
+      provider: 'appdmg',
+      source: 'http://download.adobe.com/pub/adobe/lightroom/mac/5.x/Lightroom_5_LS11_mac_5_4.dmg'
+    )
+  end
+end
