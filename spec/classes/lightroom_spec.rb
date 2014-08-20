78bc84b6e248426bf57e2507d01e3b6127f7a9af
Jatin Naik <jsnaik@gmail.com>
2014-08-20 22:11:28 +0200

    Ruby 1.8 Compatibility

--- spec/classes/lightroom_spec.rb
+++ spec/classes/lightroom_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe 'lightroom' do
   it do
     should contain_package('Lightroom').with(
-      provider: 'appdmg',
-      source: 'http://download.adobe.com/pub/adobe/lightroom/mac/5.x/Lightroom_5_LS11_mac_5_4.dmg'
+      :provider => 'appdmg',
+      :source => 'http://download.adobe.com/pub/adobe/lightroom/mac/5.x/Lightroom_5_LS11_mac_5_4.dmg'
     )
   end
 end
