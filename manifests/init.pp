48f2129cff127d3c633322471b9d838634df4eb6
Jatin Naik <jsnaik@gmail.com>
2014-08-25 23:07:30 +0200

    Parameterized the dmg version

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,9 +3,12 @@
 # Examples
 #
 #   include lightroom
-class lightroom {
+class lightroom(
+    $major_version = '5',
+    $minor_version = 'LS11_mac_5_4'
+  ) {
   package { 'Lightroom':
     provider => 'appdmg',
-    source   => 'http://download.adobe.com/pub/adobe/lightroom/mac/5.x/Lightroom_5_LS11_mac_5_4.dmg',
+    source   => "http://download.adobe.com/pub/adobe/lightroom/mac/${major_version}.x/Lightroom_${major_version}_${minor_version}.dmg",
   }
 }
